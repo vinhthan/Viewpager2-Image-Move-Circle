84067b83a5ccdf20d08ca2d97fd624cc55fec321
thanmanhvinh <thanmanhvinh@gmail.com>
2020-05-28 16:11:44 +0700

    cm

---
+++ app/src/main/java/com/example/viewpage2/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.viewpage2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager2 viewPager2;
+    private Handler slideHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager2 = findViewById(R.id.viewPageImageSlide);
+
+        //call api here
+
+        final List<SlideItem> slideItems = new ArrayList<>();
+        slideItems.add(new SlideItem(R.drawable.angry2));
+        slideItems.add(new SlideItem(R.drawable.angry4));
+        slideItems.add(new SlideItem(R.drawable.angry5));
+        slideItems.add(new SlideItem(R.drawable.austallya));
+        slideItems.add(new SlideItem(R.drawable.background_qr));
+        slideItems.add(new SlideItem(R.drawable.cast1));
+
+        viewPager2.setAdapter(new SlideAdapter(slideItems, viewPager2));
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                /*float f = 1 - Math.abs(position);
+                page.setScaleY(0.85f + f * 0.15f);*/
+            }
+        });
+        viewPager2.setPageTransformer(compositePageTransformer);
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                slideHandler.removeCallbacks(slideRunable);
+                slideHandler.postDelayed(slideRunable, 100);//
+            }
+        });
+
+
+    }
+
+    private Runnable slideRunable = new Runnable() {
+        @Override
+        public void run() {
+            viewPager2.setCurrentItem(viewPager2.getCurrentItem() + 1);
+        }
+    };
+
+}
---
+++ app/src/main/java/com/example/viewpage2/SlideItem.java
@@ -0,0 +1,14 @@
+package com.example.viewpage2;
+
+public class SlideItem {
+
+    //neu muon nhan image tu internet
+    private int image;
+    SlideItem(int image){
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
